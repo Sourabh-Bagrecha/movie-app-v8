c06071471af739c84ece3f9545eeebef2a49a483
Sourabh Bagrecha <bagrechasourabh622@gmail.com>
2023-09-07 15:50:49 +0530

    navbar creation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,16 @@
+import requests from "../src/app/utils/requests"; 
+// import { useRouter } from "next/router";
+
 export default function Navbar()
 {
+  // const router = useRouter()
   return (
-      <div>
-      <h1> Navbar </h1>  
-      </div>
-  )
+    <div className="flex justify-center bg-gray-500 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+    <h2 /* onClick={() =>(router.push(`?genre=${key}`))} */className="m-6 cursor-pointer hover:text-black active:text-yellow-300" key={key}>{title}</h2>
+    
+    ))}
 
+    </div>
+  );
 }
